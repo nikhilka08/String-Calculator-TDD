feb6b1d6d768ea6a7a7b68e8e879466e6dd317f1
totoro <nikhilka585@gmail.com>
2022-03-30 03:13:58 +0530

    Basic calculator base

--- index.test.ts
+++ index.test.ts
@@ -1,9 +1,17 @@
 import add from "./index";
 
 describe("String addition calculator", () => {
-  test("Given an empty string, it should return 0", () => {
-    const result = add("");
-    const expectedValue = 0;
-    expect(result).toBe(expectedValue);
+  describe("Basic calculator", () => {
+    test("Given an empty string, it should return 0", () => {
+      const result = add("");
+      const expectedValue = 0;
+      expect(result).toBe(expectedValue);
+    });
+
+    test("Given a single string, it should return number", () => {
+      const result = add("2");
+      const expectedValue = 2;
+      expect(result).toBe(expectedValue);
+    });
   });
 });
--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
-export default function add(numberStr: string) {
-  
+export default function add(numberStr: string): number {
+  if (numberStr === "") {
+    return 0;
+  }
+  return parseInt(numberStr);
 }
